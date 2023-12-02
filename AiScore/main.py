1fc154ec4e3efe39aa0a5b891562551b2e812358
Danial <147624287+danial2007-btk@users.noreply.github.com>
2023-12-02 16:36:47 +0500

    AiScore API

---
+++ AiScore/main.py
@@ -0,0 +1,21 @@
+from dataLoader import dataGather, collection
+from AiScore import AIScoreInput, AIScoreCalculator
+
+def AiScoreMain(car_id):
+        
+    car_id = car_id
+
+    # Assuming gathered_data is the result of dataGather function
+    aiscore_input = dataGather(collection, car_id)
+
+    # Create an instance of AIScoreCalculator
+    aiscore_calculator = AIScoreCalculator()
+
+    # Calculate AI Score
+    ai_score = aiscore_calculator.calculate_ai_score(aiscore_input)
+
+    # Print the AI Score
+    # print("AI Score: %.4f" % ai_score)
+    ai_score_rounded = round(ai_score, 4)
+
+    return ai_score_rounded
