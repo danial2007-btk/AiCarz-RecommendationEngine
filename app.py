e37863bf8dd51ca9daa008c168fe045c56db2611
hurairah4321 <151510774+hurairah4321@users.noreply.github.com>
2024-02-06 16:55:18 +0500

    Update app.py( add tire checker)

--- app.py
+++ app.py
@@ -299,7 +299,80 @@ try:
         except Exception as e:
             # Handle exceptions, log them, and return an appropriate response
             raise HTTPException(status_code=500, detail="Internal Server Error") from e
-    
+
+# ======================= Tire Checker =======================
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from pydantic import BaseModel, validator
+from tireTrade import preprocess_image, predict_image
+
+from fastapi import FastAPI, Request, HTTPException
+from starlette.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+app = FastAPI()
+
+class CatchLargeUploadMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        # Attempt to catch large uploads
+        if "content-length" in request.headers:
+            content_length = int(request.headers["content-length"])
+            max_size = 5 * 1024 * 1024  # 5 MB
+            if content_length > max_size:
+                return JSONResponse(
+                    status_code=413,
+                    content={"message": "Please upload a file of maximum 5 MB."}
+                )
+        response = await call_next(request)
+        return response
+
+# Add the middleware to the application
+app.add_middleware(CatchLargeUploadMiddleware)
+
+
+
+# Define a BaseModel for the file upload request
+class FileUpload(BaseModel):
+    file: UploadFile
+
+
+    # Custom validator to check file format
+    @validator('file')
+    def check_file_format(cls, v):
+        allowed_formats = ["image/png", "image/jpeg", "image/jpg"]
+        if v.content_type not in allowed_formats:
+            raise ValueError("Invalid file type. Only PNG, JPG, and JPEG are allowed.")
+        return v
+
+@app.get("/")
+async def rootMsg():
+    return "API IS RUNNING PERFECTLY"
+
+@app.post("/tirechecker")
+async def predict(file: UploadFile = File(...)):
+    try:
+        # Wrap the file in the FileUpload model to trigger validation
+        file_upload = FileUpload(file=file)
+        
+        # Check the file extension
+        # if file.content_type not in ["image/png", "image/jpeg", "image/jpg"]:
+        #     raise HTTPException(status_code=400, ="Invalid file type. Only PNG, JPG, and JPEG are allowed.")
+
+        # Read image file
+        contents = await file.read()
+
+        # Preprocess the image
+        img_array = preprocess_image(contents)
+
+        # Make prediction
+        result = predict_image(img_array)
+
+        return {"result": result}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
     
 except Exception as e:
     print(e) 
