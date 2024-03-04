243cc19dc1bba9daa441928dbedfb1d6e8cb5d57
danial2007-btk <danial.btkit@gmail.com>
2024-03-04 16:44:57 +0500

    code update

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from fastapi import FastAPI, HTTPException, Depends, Query, status,File, UploadFile,Request
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Query,
+    status,
+    File,
+    UploadFile,
+    Request,
+)
 from fastapi.responses import StreamingResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse
-from io import BytesIO 
+from io import BytesIO
 
 
 from contextlib import asynccontextmanager
 from pydantic import BaseModel, validator
 from bson import ObjectId
 
-from main import AiScoreMain,FeedManagerMain, modelStatsMain, LikeandDislikecount
+from main import AiScoreMain, FeedManagerMain, modelStatsMain, LikeandDislikecount
+
 # from adStatus import carAdMain
 
-from adStatus import dummy # Rempve This
-from dummyFunction import carTire # Remove This
+from adStatus import dummy  # Rempve This
+from dummyFunction import carTire  # Remove This
 
 from mongodb import mongodbConn, carzcollection, usercollection
 
-# from memory_profiler import profile 
-    
+# from memory_profiler import profile
+
 try:
-    
+
     @asynccontextmanager
     async def lifespan(app: FastAPI):
         # Load the ML model
@@ -30,8 +40,8 @@ try:
         # Clean up the ML models and release the resources
         print("xxxxxxxx   shurting down event")
         mongodbConn.close()
-        print("mongodb disconnected")    
-    
+        print("mongodb disconnected")
+
     app = FastAPI(lifespan=lifespan)
 
     # Base URL for the API
@@ -46,14 +56,12 @@ try:
             raise HTTPException(status_code=401, detail="Invalid API key")
         return api_key
 
-
     # **************************       APP CHECKING         **************************
 
     # Root Endpoint
     @app.get("/")
     def read_root():
         return {"message": "App is running successfully"}
-
 
     # **************************       AI SCORE API ENDPOINT         **************************
 
@@ -68,7 +76,7 @@ try:
         api_key: str = Depends(check_api_key, use_cache=True),
     ):
 
-    # ================== Checking Valid ObjectId for Car ID ==================
+        # ================== Checking Valid ObjectId for Car ID ==================
 
         # Check if car_id is a valid MongoDB ObjectId
         if not ObjectId.is_valid(car_data.carid):
@@ -76,8 +84,8 @@ try:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid car_id. Must be a valid MongoDB ObjectId.",
             )
-            
-    # ======================= Ai Score Calculation =======================
+
+        # ======================= Ai Score Calculation =======================
 
         try:
             # Function named AiScoreMain that takes a car_id as input
@@ -87,15 +95,14 @@ try:
         except Exception as e:
             # Handle exceptions, log them, and return an appropriate response
             raise HTTPException(status_code=500, detail="Internal Server Error") from e
-    
-    
+
     # **************************       FEED MANAGER API ENDPOINT         **************************
 
     # FeedManager API Endpoint
     class FeedManagerInput(BaseModel):
-        user_id: str # User ID as input
-        longitude: float # User longitude: float
-        latitude: float # User latitude: float
+        user_id: str  # User ID as input
+        longitude: float  # User longitude: float
+        latitude: float  # User latitude: float
 
     # @profile
     @app.post("/feedmanager")
@@ -103,43 +110,41 @@ try:
         feed_data: FeedManagerInput,
         api_key: str = Depends(check_api_key, use_cache=True),
     ):
-        
-        
-    # ======================= Checking if UserID is correct Object Id =======================
-        
+
+        # ======================= Checking if UserID is correct Object Id =======================
+
         # Check if user_id is a valid MongoDB ObjectId
         if not ObjectId.is_valid(feed_data.user_id):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid user_id. Must be a valid MongoDB ObjectId.",
             )
-            
-    # ======================= Checking if UserID is in Database or not =======================
-        
+
+        # ======================= Checking if UserID is in Database or not =======================
+
         # Check if user_id exists in the database
         if not usercollection.find_one({"_id": ObjectId(feed_data.user_id)}):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="User Id not found in database .",
             )
-            
 
-    # ===================== Validating the Longitude and Latitude is empty or not =====================
-        
+        # ===================== Validating the Longitude and Latitude is empty or not =====================
+
         # Validate input data
         if not feed_data.longitude or not feed_data.latitude:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Longitude and latitude are required fields.",
             )
-            
-    # ===================== Validating the Longitude and Latitude is empty or not =====================
-        
+
+        # ===================== Validating the Longitude and Latitude is empty or not =====================
+
         # Validate longitude and latitude input data
         try:
             longitude = float(feed_data.longitude)
             latitude = float(feed_data.latitude)
-            
+
             # Check if longitude is in the valid range [-180, 180]
             if not (-180 <= longitude <= 180):
                 raise ValueError("Longitude format is invalid.")
@@ -155,11 +160,11 @@ try:
             )
 
         try:
-            
+
             coordinates = [feed_data.longitude, feed_data.latitude]
 
             # Function named FeedManagerMain that takes a car_id and coordinates as input
-            carIds_ouput = FeedManagerMain(feed_data.user_id, coordinates)      
+            carIds_ouput = FeedManagerMain(feed_data.user_id, coordinates)
 
             return carIds_ouput
 
@@ -167,37 +172,34 @@ try:
             # Handle exceptions, log them, and return an appropriate response
             raise HTTPException(status_code=500, detail=e)
 
-
-
     # **************************       Model Stats API ENDPOINT         **************************
 
     # Model Stats Endpoint
     class modelStatsInput(BaseModel):
-        user_id: str # User ID as input
+        user_id: str  # User ID as input
 
     @app.post("/modelStats")
     async def modelStats(
         modelstats: modelStatsInput,
         api_key: str = Depends(check_api_key, use_cache=True),
-    ):  
-        longitude= 2.9916
-        latitude= 53.4048
-        
+    ):
+        longitude = 2.9916
+        latitude = 53.4048
+
         try:
-            coordinates = [longitude,latitude]
+            coordinates = [longitude, latitude]
             stats_output = modelStatsMain(modelstats.user_id, coordinates)
             return stats_output
         except Exception as e:
             # Handle exceptions, log them, and return an appropriate response
             raise HTTPException(status_code=500, detail=e)
-        
-   
+
     # **************************       Like and Dislike Count API ENDPOINT         **************************
 
     class LikeandDislikecountInput(BaseModel):
-        user_id: str # User ID as input
-        longitude: float # User longitude: float
-        latitude: float # User latitude: float
+        user_id: str  # User ID as input
+        longitude: float  # User longitude: float
+        latitude: float  # User latitude: float
 
     @app.post("/likeanddislikecount")
     async def likeanddislikecount(
@@ -205,39 +207,44 @@ try:
         api_key: str = Depends(check_api_key, use_cache=True),
     ):
         # ======================= Checking if UserID is correct Object Id =======================
-        
+
         # Check if user_id is a valid MongoDB ObjectId
         if not ObjectId.is_valid(likeanddislikecount_data.user_id):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid user_id. Must be a valid MongoDB ObjectId.",
             )
-        
+
         # ======================= Checking if UserID is in Database or not =======================
-        
+
         # Check if user_id exists in the database
-        if not usercollection.find_one({"_id": ObjectId(likeanddislikecount_data.user_id)}):
+        if not usercollection.find_one(
+            {"_id": ObjectId(likeanddislikecount_data.user_id)}
+        ):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="User Id not found in database.",
             )
-        
+
         # ===================== Validating the Longitude and Latitude is empty or not =====================
-        
+
         # Validate input data
-        if not likeanddislikecount_data.longitude or not likeanddislikecount_data.latitude:
+        if (
+            not likeanddislikecount_data.longitude
+            or not likeanddislikecount_data.latitude
+        ):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Longitude and latitude are required fields.",
             )
-        
+
         # ===================== Validating the Longitude and Latitude is empty or not =====================
 
         # Validate longitude and latitude input data
         try:
             longitude = float(likeanddislikecount_data.longitude)
             latitude = float(likeanddislikecount_data.latitude)
-            
+
             # Check if longitude is in the valid range [-180, 180]
             if not (-180 <= longitude <= 180):
                 raise ValueError("Longitude format is invalid.")
@@ -251,27 +258,75 @@ try:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid longitude or latitude format.",
             )
-            
+
         try:
-            coordinates = [likeanddislikecount_data.longitude, likeanddislikecount_data.latitude]
-            likeanddislikecount_output = LikeandDislikecount(likeanddislikecount_data.user_id, coordinates)
+            coordinates = [
+                likeanddislikecount_data.longitude,
+                likeanddislikecount_data.latitude,
+            ]
+            likeanddislikecount_output = LikeandDislikecount(
+                likeanddislikecount_data.user_id, coordinates
+            )
             return likeanddislikecount_output
 
         except Exception as e:
             # Handle exceptions, log them, and return an appropriate response
             raise HTTPException(status_code=500, detail=e)
-        
 
-# **************************       Car Body Pannel Gap API ENDPOINT         **************************
+    # **************************       Car AD Checker API ENDPOINT         **************************
+
+    class AdCarIdInput(BaseModel):
+        carid: str  # Car ID as input
+
+    # car AdChecking API Endpoint
+    # @profile
+    @app.post("/adChecker")
+    async def car_ad_checker(
+        car_data: AdCarIdInput,
+        api_key: str = Depends(check_api_key, use_cache=True),
+    ):
+
+        # ================== Checking Valid ObjectId for Car ID ==================
+
+        # Check if car_id is a valid MongoDB ObjectId
+        if not ObjectId.is_valid(car_data.carid):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid car_id. Must be a valid MongoDB ObjectId.",
+            )
+
+        # ======================= Checking if UserID is in Database or not =======================
+
+        # Check if user_id exists in the database
+        if not carzcollection.find_one({"_id": ObjectId(car_data.carid)}):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User Id not found in database.",
+            )
+
+        # ======================= Ad Checking =======================
+
+        try:
+            # car_ad_score = carAdMain(car_data.carid)
+            car_ad_score = dummy(car_data.carid)
+            return car_ad_score
+
+        except Exception as e:
+            # Handle exceptions, log them, and return an appropriate response
+            raise HTTPException(status_code=500, detail="Internal Server Error") from e
+
+    # **************************       Car Body Pannel Gap API ENDPOINT         **************************
     class PanelInput(BaseModel):
         file: UploadFile
-    
+
         # Custom validator to check file format
         @validator("file")
         def check_file_format(cls, v):
             allowed_formats = ["image/png", "image/jpeg", "image/jpg"]
             if v.content_type not in allowed_formats:
-                raise ValueError("Invalid file type: Only PNG, JPG, and JPEG are allowed.")
+                raise ValueError(
+                    "Invalid file type: Only PNG, JPG, and JPEG are allowed."
+                )
             return v
 
     @app.post("/panelgap")
@@ -283,13 +338,14 @@ try:
             # Access the uploaded file using panel_input.file
             contents = await panel_input.file.read()
             # Returning the image as a StreamingResponse
-            return StreamingResponse(BytesIO(contents), media_type=panel_input.file.content_type)
+            return StreamingResponse(
+                BytesIO(contents), media_type=panel_input.file.content_type
+            )
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
-# **************************       Car Tier Tread API ENDPOINT         **************************
-
+    # **************************       Car Tier Tread API ENDPOINT         **************************
 
     class CatchLargeUploadMiddleware(BaseHTTPMiddleware):
         async def dispatch(self, request: Request, call_next):
@@ -344,49 +400,6 @@ try:
             raise HTTPException(status_code=500, detail=str(e))
 
 
-    # **************************       Car AD Checker API ENDPOINT         **************************
 
-    class AdCarIdInput(BaseModel):
-        carid: str  # Car ID as input
-
-    # car AdChecking API Endpoint
-    # @profile
-    @app.post("/adChecker")
-    async def car_ad_checker(
-        car_data: AdCarIdInput,
-        api_key: str = Depends(check_api_key, use_cache=True),
-    ):
-
-    # ================== Checking Valid ObjectId for Car ID ==================
-
-        # Check if car_id is a valid MongoDB ObjectId
-        if not ObjectId.is_valid(car_data.carid):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Invalid car_id. Must be a valid MongoDB ObjectId.",
-            )
-            
-    # ======================= Checking if UserID is in Database or not =======================
-        
-        # Check if user_id exists in the database
-        if not carzcollection.find_one({"_id": ObjectId(car_data.carid)}):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="User Id not found in database.",
-            )
-            
-    # ======================= Ad Checking =======================
-
-        try:
-                # car_ad_score = carAdMain(car_data.carid)
-                car_ad_score = dummy(car_data.carid)
-                return car_ad_score
-
-        except Exception as e:
-            # Handle exceptions, log them, and return an appropriate response
-            raise HTTPException(status_code=500, detail="Internal Server Error") from e
-    
-    
 except Exception as e:
-    print(e) 
-
+    print(e)
